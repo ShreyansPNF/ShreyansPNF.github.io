74de06148e99eb721a725e0604791e2261d95cee
Shreyans Sharma <1241P1@pathshala.edu.np>
2026-08-10 15:41:00 +0545

    Delete carousel.js

--- carousel.js
+++
@@ -1,5 +0,0 @@
-function scrollCarousel(direction) {
-  const carousel = document.getElementById("carousel");
-  const scrollAmount = carousel.offsetWidth * 0.8;
-  carousel.scrollLeft += direction * scrollAmount;
-}
